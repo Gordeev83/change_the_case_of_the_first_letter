d10d3b0732087f4d38c47a92478ec15013d55e30
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 12:41:12 +0300

    Добавьте файлы проекта.

---
+++ change_the_case_of_the_first_letter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace change_the_case_of_the_first_letter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите текст:");
+            string input = Console.ReadLine();
+
+            string result = Regex.Replace(input, @"(^\w|\.\s+\w)", m => m.Value.ToUpper());
+
+            Console.WriteLine("Результат:");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
